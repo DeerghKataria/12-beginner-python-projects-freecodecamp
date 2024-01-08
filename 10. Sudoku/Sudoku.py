d7279b8f5a546822713c21f5818a481babcc763e
DeerghKataria <deerghkataria@gmail.com>
2024-01-08 23:47:56 +0530

    backtracking will be used with sudoku

---
+++ 10. Sudoku/Sudoku.py
@@ -0,0 +1,7 @@
+def solve_sudoku(puzzle):
+    # We will be solving sudoku using backtracking
+    # We will be using a list of lists since lists are mutable (i.e. they can be changed later)
+    # Returning a solution
+    # Mutates puzzle to the solution if the solution exists.
+    
+    
