a30ef113b624e63e845200b6169312e678770def
DeerghKataria <deerghkataria@gmail.com>
2024-01-11 23:47:06 +0530

    Function for checking the conditon has been made

--- 10. Sudoku/Sudoku.py
+++ 10. Sudoku/Sudoku.py
@@ -10,6 +10,37 @@ def find_next_empty(puzzle):
                 return r, c
     return None, None   # This means that there are no spaces in the puzzle leftk
 
+def is_valid(puzzle, guess, row, col):
+    # Figures out whether the guess at that row/col is a valid guess or not?
+    # returns True if is valid, False otherwise
+
+    # Let's start with the row:
+    row_vals = puzzle[row]
+    if guess in row_vals:
+        return False
+    
+    # Now the columns
+    # col_vals = []
+    # for i in range(9):
+    #     col_vals.append(puzzle[i][col])
+
+    col_vals = [puzzle[i][col] for i in range(9)]
+    if guess in col_vals:
+        return False
+    
+    # and then for square you have to iterate over the 3x3 values in the row/col
+
+    row_start = (row // 3)  # 1 // 3 = 0, 5 // 3 = 1, ...
+    col_start = (col // 3)
+
+    for r in range(row_start, row_start + 3):
+        for c in range(col_start, col_start + 3):
+            if puzzle[r][c] == guess:
+                return False
+            
+    # If we get here, these checks pass
+    return True
+    
 def solve_sudoku(puzzle):
     # We will be solving sudoku using backtracking
     # We will be using a list of lists since lists are mutable (i.e. they can be changed later)
@@ -24,5 +55,8 @@ def solve_sudoku(puzzle):
         return True
     
     # Step 2: If there is a place to put a number, then we can make a guess between 1 and 9
-    for guess in range(1, 10)   # 1, 2, 3.....9
-        # Step 3: To check whether this is a valid guess or not 
+    for guess in range(1, 10):   # 1, 2, 3.....9
+        # Step 3: To check whether this is a valid guess or not
+        if is_valid(puzzle, guess, row, col):
+
+    
